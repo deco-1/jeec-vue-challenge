f3bfe1d23217a005956d69540a30a1a5f79469ec
deco <andrefsantos@skiff.com>
2024-04-04 02:00:10 +0100

    Revert "build for gh-pages"
    This reverts commit 91d7e2581fd306fd81ec626eed1cfe67f0ca6afc.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/jeec-vue-challenge/" : "/",
   transpileDependencies: true
 })
