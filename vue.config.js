91d7e2581fd306fd81ec626eed1cfe67f0ca6afc
deco <andrefsantos@skiff.com>
2024-04-04 01:54:49 +0100

    build for gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/jeec-vue-challenge/" : "/",
   transpileDependencies: true
 })
